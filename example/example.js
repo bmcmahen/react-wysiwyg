bd6eec15f0a4a4cbf2a7853cddd548d18718e722
bmcmahen <ben.mcmahen@gmail.com>
2015-08-04 10:50:28 -0600

    make cool twitter style demo, with gif

--- example/example.js
+++ example/example.js
@@ -5,37 +5,81 @@ var Example = React.createClass({
 
   getInitialState: function(){
     var editing = false
+    var defaultValue = ''
 
     return {
-      html: 'default value',
-      editing: false,
-      placeholder: false
+      html: defaultValue,
+      editing: true,
+      placeholder: true,
+      maxLength: 140,
+      totalLength: defaultValue.length,
+      queryMention: false
     }
 
   },
 
+  componentDidMount () {
+    window.requestAnimationFrame(this.checkCursor)
+  },
+
+  checkCursor (timestamp) {
+    var self = this
+    var selection = window.getSelection()
+
+
+    if (this.state.editing && selection.focusNode) {
+
+      var node = selection
+        .getRangeAt(0)
+        .commonAncestorContainer
+        .parentNode
+
+      if (node.className === 'show-dropdown') {
+        // you could use the node to determine its position,
+        // and show the dropdown inline, too.
+        this.setState({ queryMention : node.textContent })
+      } else {
+        this.setState({ queryMention: false })
+      }
+
+    } else {
+      this.setState({ queryMention: false })
+    }
+
+    window.requestAnimationFrame(self.checkCursor)
+  },
+
   render: function(){
+
+    var isValid = (this.state.maxLength >= this.state.totalLength)
+      && (this.state.totalLength > 0)
+
     return (
-      <div>
+      <div className='container'>
         <div aria-live='polite'>{this.state.error}</div>
         <ContentEditable
           ref='editable'
           tagName='div'
           html={this.state.html}
           placeholder={this.state.placeholder}
-          placeholderText='Your Name'
+          placeholderText='Compose Your Tweet'
           onKeyPress={this.onKeyPress}
           preventStyling
           noLinebreaks
           onChange={this.onChange}
           editing={this.state.editing}
         />
-        <button onClick={this.enableEditing}>
-          {!this.state.editing ? 'Enable Editing' : 'Finish Editing'}
-        </button>
-        <button onClick={this.autofocus}>
-          autofocus if editing
-        </button>
+        <div className='controls'>
+          <div>
+            {this.state.maxLength - this.state.totalLength}
+          </div>
+          <button disabled={!isValid} onClick={this.enableEditing}>
+            Tweet
+          </button>
+        </div>
+        <div>
+          Show autocomplete? {this.state.queryMention ? 'Yes ' + this.state.queryMention : 'No'}
+        </div>
       </div>
     );
   },
@@ -46,40 +90,41 @@ var Example = React.createClass({
     }
   },
 
-  onKeyPress: function (e) {
-    var val = e.target.textContent;
-
-    // max-length validation on the fly
-    if (val.length > 30) {
-      e.preventDefault();
-      e.stopPropagation();
-      this.setState({ error: 'Max length is 30 characters'})
-    } else {
-      this.setState({ error: null })
-    }
-
-  },
-
-  onChange: function(html, setPlaceholder) {
+  onChange: function(text, setPlaceholder) {
     // in order to render the updated html,
     // you need to pass it as a prop to contentEditable.
     // This gives you increased flexibility.
     if (setPlaceholder) {
       this.setState({
         placeholder: true,
-        html: ''
+        html: '',
+        totalLength: 0
       })
     } else {
 
+      var copy = text.slice(0, this.state.maxLength)
 
+      // mutations
+      // @name
       var regex = /(^|[^@\w])@(\w{1,15})\b/g
-      var replace = '$1<a href="http://twitter.com/$2">@$2</a>'
-      var output = html.replace(regex, replace)
+      var replace = '$1<a class="show-dropdown" href="http://twitter.com/$2">@$2</a>'
+      var output = copy.replace(regex, replace)
 
+      // #hash
+      output = output.replace(/(^|\W)(#[a-z\d][\w-]*)/ig, '$1<span class="show-dropdown" style="color: #aaa;">$2</span>')
+
+      // text overflow
+      if (text.length > this.state.maxLength) {
+        var overflow = '<span style="color: #bbb; background-color: #eee; text-decoration: line-through;">' +
+          text.slice(this.state.maxLength) +
+          '</span>'
+        output = output + overflow
+      }
 
       this.setState({
         placeholder: false,
-        html: output
+        html: output,
+        totalLength: text.length
       })
     }
 
